ea10bfa6f1e9cbe765d0aa88e9d1835c09029880
teawon <xodnjsvkvk9@gmail.com>
2023-11-14 02:43:15 +0900

    test: DiscountManager 테스트 코드 작성

---
+++ __tests__/DiscountManager.js
@@ -0,0 +1,72 @@
+import DiscountManager from '../src/model/DiscountManager';
+import ChristmasDdayDiscount from '../src/model/DisCountEvent/ChristmasDdayDiscount';
+import GiftDiscount from '../src/model/DisCountEvent/GiftDiscount';
+
+import Money from '../src/model/Money';
+import Food from '../src/model/Food';
+import CustomDate from '../src/model/CustomDate';
+import Order from '../src/model/Order';
+
+describe('DiscountManager 클래스 테스트', () => {
+  const discount_events = [new ChristmasDdayDiscount(), new GiftDiscount()];
+  const food1 = new Food('양송이수프', new Money(6000), '애피타이저');
+  const food2 = new Food('티본스테이크', new Money(55000), '메인');
+  const date = new CustomDate(2023, 12, 15);
+  const foodList = [
+    { food: food1, count: 2 },
+    { food: food2, count: 5 },
+  ];
+  const order = new Order(foodList, date);
+  const FREE_GIFT = new Food('샴페인', new Money(25000), '음료');
+
+  describe('DiscountManager 모델 테스트', () => {
+    test('적용 가능한 할인 이벤트의 결과를 반환한다', () => {
+      // given
+      const discountManager = new DiscountManager(order, discount_events);
+
+      // when
+      const discountResult = discountManager.getDiscountResults();
+
+      // then
+      expect(discountResult).toStrictEqual([
+        {
+          name: '크리스마스 디데이 할인',
+          content: {
+            money: new Money(2000),
+          },
+        },
+        {
+          name: '증정 이벤트',
+          content: {
+            gift: FREE_GIFT,
+          },
+        },
+      ]);
+    });
+
+    test('이벤트를 통해 증졍되는 아이템 요소를 반환한다', () => {
+      // given
+      const discountManager = new DiscountManager(order, discount_events);
+      const discountResult = discountManager.getDiscountResults();
+
+      // when
+      const giftList = discountManager.getGifts(discountResult);
+
+      // when
+      expect(giftList).toStrictEqual([FREE_GIFT]);
+    });
+
+    test('총 혜택 금액을 반환한다', () => {
+      // given
+      const discountManager = new DiscountManager(order, discount_events);
+      const discountResult = discountManager.getDiscountResults();
+
+      // when
+      const discountMoney =
+        discountManager.calculateTotalDiscountMoney(discountResult);
+
+      // then
+      expect(discountMoney.getPrice()).toBe(25000 + 2000);
+    });
+  });
+});
