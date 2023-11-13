9860d3f769fd07b533fef89b05d05d59ba7ab49c
teawon <xodnjsvkvk9@gmail.com>
2023-11-13 23:26:49 +0900

    feat : 평일 할인 클래스 구현

---
+++ src/model/DisCountEvent/WeekdayDiscount.js
@@ -0,0 +1,53 @@
+import DiscountEvent from './DiscountEvent';
+import CustomDate from '../CustomDate';
+import Money from '../Money';
+
+class WeekdayDiscount extends DiscountEvent {
+  static DISCOUNT_MENU_TYPE = '디저트';
+  static DISCOUNT_AMOUNT = 2023;
+
+  #eventName;
+  #appliedPeriod;
+
+  constructor() {
+    super();
+    this.#eventName = '평일 할인';
+    this.#appliedPeriod = {
+      start: new CustomDate(2023, 12, 1),
+      end: new CustomDate(2023, 12, 31),
+    };
+  }
+
+  isApplicable(order) {
+    const orderDate = order.getDate();
+
+    if (
+      !orderDate.isBetween(this.#appliedPeriod.start, this.#appliedPeriod.end)
+    )
+      return false;
+    if (order.getTotalMoney().getPrice() < 10000) return false;
+
+    if (!orderDate.isWeekday()) return false;
+
+    return true;
+  }
+
+  getDiscountDetails(order) {
+    const disCountTypeCount = order.getTotalOrderCountByType(
+      WeekdayDiscount.DISCOUNT_MENU_TYPE,
+    );
+
+    const disCountMoney = new Money(WeekdayDiscount.DISCOUNT_AMOUNT).multiply(
+      disCountTypeCount,
+    );
+
+    return {
+      name: this.#eventName,
+      content: {
+        money: disCountMoney,
+      },
+    };
+  }
+}
+
+export default WeekdayDiscount;
