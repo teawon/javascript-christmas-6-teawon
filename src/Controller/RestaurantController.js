2cb38ee1a85c0aabfb73d65de552a211cc4a8b5f
teawon <xodnjsvkvk9@gmail.com>
2023-11-15 01:43:37 +0900

    refactor : 컨트롤러 로직 분리

--- src/Controller/RestaurantController.js
+++ src/Controller/RestaurantController.js
@@ -9,71 +9,116 @@ import GiftDiscount from '../model/DisCountEvent/GiftDiscount.js';
 import WeekdayDiscount from '../model/DisCountEvent/WeekdayDiscount.js';
 import WeekendDiscount from '../model/DisCountEvent/WeekendDiscount.js';
 import SpecialDayDiscount from '../model/DisCountEvent/SpecialDayDiscount.js';
+import OutputView from '../Views/OutputView.js';
 import { Console } from '@woowacourse/mission-utils';
 
 class RestaurantController {
-  async start() {
-    Console.print('안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.');
+  #discountEvents;
 
-    const dateInput = await Console.readLineAsync(
-      '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)',
-    );
-
-    const visitDate = new CustomDate(2023, 12, Number(dateInput));
-
-    const orderMenuInput = await Console.readLineAsync(
-      '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)',
-    );
-
-    const orderMenuList = orderMenuInput.split(',');
-
-    const orderMenu = orderMenuList.map((menu) => {
-      const [name, count] = menu.split('-');
-      const food = Menu.getFood(name);
-      return { food, count: Number(count) };
-    });
-
-    const order = new Order(orderMenu, visitDate);
-
-    Console.print('12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!');
-
-    Console.print('<주문 메뉴>');
-
-    orderMenu.forEach((item) => {
-      Console.print(`${item.food.getName()} ${item.count}개`);
-    });
-
-    Console.print('<할인 전 총주문 금액>');
-
-    Console.print(`${order.getTotalMoney().getPrice()}원`);
-
-    const discountEvents = [
+  constructor() {
+    this.#discountEvents = [
       new ChristmasDdayDiscount(),
       new GiftDiscount(),
       new WeekdayDiscount(),
       new WeekendDiscount(),
       new SpecialDayDiscount(),
     ];
+  }
 
-    const discountManager = new DiscountManager(order, discountEvents);
+  async start() {
+    OutputView.printWelcome();
+    const visitDate = await this.#getVisitDate();
+    const orderMenu = await this.#getOrderMenu();
+    const order = new Order(orderMenu, visitDate);
+
+    this.#printEventPreview(visitDate);
+    this.#printMenu(order);
+    this.#printTotalOrderPrice(order);
+
+    const { totalProfitMoney, totalDiscountMoney } =
+      this.#calculateAndPrintDiscounts(order);
+
+    this.#printFinalPaymentPrice(order, totalDiscountMoney);
+
+    const eventBadge = EventBadgeManager.getEventBadge(totalProfitMoney);
+    this.#printEventBadge(eventBadge);
+  }
+
+  #calculateAndPrintDiscounts(order) {
+    const discountManager = new DiscountManager(order, this.#discountEvents);
     const discountResult = discountManager.getDiscountResults();
-
-    Console.print('<증정 메뉴>');
-
     const gifts = discountManager.getGifts(discountResult);
 
-    if (gifts.length === 0) {
-      Console.print('없음');
-    } else {
-      const giftDescriptions = gifts
-        .map((gift) => `${gift.gift.getName()} ${gift.count}개`)
-        .join(', ');
-      Console.print(giftDescriptions);
-    }
-    Console.print('<혜택 내역>');
-    discountResult.forEach((discount) => {
-      let money = new Money(0);
+    this.#printGiftMenu(gifts);
+    this.#printBenefitDetails(discountResult);
 
+    const { totalDiscountMoney, totalGiftMoney } =
+      discountManager.calculateTotalDiscount(discountResult);
+
+    const totalProfitMoney = totalDiscountMoney.add(totalGiftMoney);
+    this.#printTotalBenefit(totalProfitMoney);
+
+    return { totalDiscountMoney, totalProfitMoney };
+  }
+
+  async #getVisitDate() {
+    const dateInput = await Console.readLineAsync(
+      '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)',
+    );
+
+    //TODO Validation 및 재 입력받기
+    const visitDate = new CustomDate(2023, 12, Number(dateInput));
+
+    return visitDate;
+  }
+
+  async #getOrderMenu() {
+    const orderMenuInput = await Console.readLineAsync(
+      '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)',
+    );
+
+    const orderMenuList = orderMenuInput.split(',');
+    const orderMenu = orderMenuList.map((menu) => {
+      const [name, count] = menu.split('-');
+      const food = Menu.getFood(name);
+      return { food, count: Number(count) };
+    });
+    //TODO Validation 및 재 입력받기
+    return orderMenu;
+  }
+
+  #printMenu(order) {
+    const foodList = order.getFoodList();
+    const orderMenuData = foodList.map((foodList) => ({
+      name: foodList.food.getName(),
+      count: foodList.count,
+    }));
+    OutputView.printMenu(orderMenuData);
+  }
+
+  #printEventPreview(visitDate) {
+    const month = visitDate.getMonth();
+    const day = visitDate.getDay();
+    OutputView.printEventPreview(month, day);
+  }
+
+  #printTotalOrderPrice(order) {
+    const totalOrderMoney = order.getTotalMoney();
+    const totalOrderPrice = totalOrderMoney.getPrice();
+    OutputView.printTotalOrderPrice(totalOrderPrice);
+  }
+
+  #printGiftMenu(gifts) {
+    const giftsData = gifts.map((item) => ({
+      name: item.gift.getName(),
+      count: item.count,
+    }));
+    OutputView.printGiftMenu(giftsData);
+  }
+
+  #printBenefitDetails(discountResult) {
+    const discountDatas = discountResult.map((discount) => {
+      let money = new Money(0);
       if (discount.content.gift) {
         money = money.add(discount.content.gift.getMoney());
       }
@@ -81,33 +126,28 @@ class RestaurantController {
         money = money.add(discount.content.money);
       }
 
-      Console.print(`${discount.name}: -${money.getPrice()}원`);
+      return { name: discount.name, amount: money };
     });
+    OutputView.printBenefitDetails(discountDatas);
+  }
 
-    Console.print('<총혜택 금액>');
+  #printTotalBenefit(totalProfitMoney) {
+    const totalProfitPrice = totalProfitMoney.getPrice();
+    OutputView.printTotalBenefit(totalProfitPrice);
+  }
 
-    const { totalDiscountMoney, totalGiftMoney } =
-      discountManager.calculateTotalDiscount(discountResult);
+  #printFinalPaymentPrice(order, totalDiscountMoney) {
+    const finalPaymentMoney = order.getTotalMoney().minus(totalDiscountMoney);
+    const finalPaymentPrice = finalPaymentMoney.getPrice();
+    OutputView.printFinalPaymentPrice(finalPaymentPrice);
+  }
 
-    const totalProfitMoney = totalDiscountMoney.add(totalGiftMoney);
-
-    Console.print(`-${totalProfitMoney.getPrice()}원`);
-
-    Console.print('<할인 후 예상 결제 금액>');
-
-    const totalMoney = order.getTotalMoney().minus(totalDiscountMoney);
-
-    Console.print(`${totalMoney.getPrice()}원`);
-
-    Console.print('<12월 이벤트 배지>');
-
-    const eventBadge = EventBadgeManager.getEventBadge(totalProfitMoney);
-
+  #printEventBadge(eventBadge) {
+    let eventBadgeName = '';
     if (eventBadge) {
-      Console.print(eventBadge.getName());
-    } else {
-      Console.print('없음');
+      eventBadgeName = eventBadge.getName();
     }
+    OutputView.printEventBadge(eventBadgeName);
   }
 }
 
