bdf5cbc1e40d074421389e0fbaf14e79192e0c09
teawon <xodnjsvkvk9@gmail.com>
2023-11-15 04:24:15 +0900

    refactor : 입력 메세지 상수 분리

--- src/Views/InputView.js
+++ src/Views/InputView.js
@@ -1,16 +1,15 @@
 import { Console } from '@woowacourse/mission-utils';
+import { INPUT_MESSAGE } from '../message.js';
 
 const InputView = {
   async readDate() {
-    const dateInput = await Console.readLineAsync(
-      '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n',
-    );
+    const dateInput = await Console.readLineAsync(INPUT_MESSAGE.enterVisitDate);
     return dateInput;
   },
 
   async readOrderMenu() {
     const orderMenuInput = await Console.readLineAsync(
-      '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n',
+      INPUT_MESSAGE.enterOrderMenu,
     );
     return orderMenuInput;
   },
--- src/message.js
+++ src/message.js
@@ -17,3 +17,10 @@ export const OUTPUT_MESSAGE = Object.freeze({
   eventBadgeHeader: '<12월 이벤트 배지>',
   emptyLine: '',
 });
+
+export const INPUT_MESSAGE = Object.freeze({
+  enterVisitDate:
+    '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n',
+  enterOrderMenu:
+    '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n',
+});
