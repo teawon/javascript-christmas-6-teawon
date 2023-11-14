1af64a24a1ac81c57de64792f18891f7ea6c1e11
teawon <xodnjsvkvk9@gmail.com>
2023-11-15 01:55:12 +0900

    refactor : InputView 입력 로직 분리

--- src/Controller/RestaurantController.js
+++ src/Controller/RestaurantController.js
@@ -10,6 +10,7 @@ import WeekdayDiscount from '../model/DisCountEvent/WeekdayDiscount.js';
 import WeekendDiscount from '../model/DisCountEvent/WeekendDiscount.js';
 import SpecialDayDiscount from '../model/DisCountEvent/SpecialDayDiscount.js';
 import OutputView from '../Views/OutputView.js';
+import InputView from '../Views/InputView.js';
 import { Console } from '@woowacourse/mission-utils';
 
 class RestaurantController {
@@ -62,9 +63,7 @@ class RestaurantController {
   }
 
   async #getVisitDate() {
-    const dateInput = await Console.readLineAsync(
-      '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)',
-    );
+    const dateInput = await InputView.readDate();
 
     //TODO Validation 및 재 입력받기
     const visitDate = new CustomDate(2023, 12, Number(dateInput));
@@ -73,9 +72,7 @@ class RestaurantController {
   }
 
   async #getOrderMenu() {
-    const orderMenuInput = await Console.readLineAsync(
-      '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)',
-    );
+    const orderMenuInput = await InputView.readOrderMenu();
 
     const orderMenuList = orderMenuInput.split(',');
     const orderMenu = orderMenuList.map((menu) => {
